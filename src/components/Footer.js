63794e61a293617b3b0350e2b43fa395f61afe78
Fred-S <fredvantage@gmail.com>
2020-09-06 10:15:35 +0200

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,61 @@
 import React from 'react'
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <section id="footer">
-                <ul className="icons">
-                    <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                    <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                    <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
-                </ul>
-                <ul className="copyright">
-                    <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                </ul>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section id="footer">
+        <ul className="icons">
+          {/* <li>
+            <a href="#" className="icon alt fa-twitter">
+              <span className="label">Twitter</span>
+            </a>
+          </li> */}
+          {/* <li>
+            <a href="#" className="icon alt fa-facebook">
+              <span className="label">Facebook</span>
+            </a>
+          </li> */}
+          <li>
+            <a
+              href="https://www.instagram.com/fredericsoloy/"
+              target="_blank"
+              className="icon alt fa-instagram"
+            >
+              <span className="label">Instagram</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/Frederic-S"
+              target="_blank"
+              className="icon alt fa-github"
+            >
+              <span className="label">GitHub</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/fred-soloy/"
+              target="_blank"
+              className="icon alt fa-linkedin"
+            >
+              <span className="label">Linkedin</span>
+            </a>
+          </li>
+        </ul>
+        <ul className="copyright">
+          <li>&copy; Facts Tab</li>
+          <li>
+            Fred Soloy{' '}
+            <a href="https://fredsoloy.com" target="_blank">
+              {' '}
+              My Site
+            </a>
+          </li>
+        </ul>
+      </section>
+    )
+  }
 }
 
 export default Footer
